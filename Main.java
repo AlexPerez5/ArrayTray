e19ccc90cdeee90407457d2a5fd391eb53145d93
Alexander Perez <alexander.perez@patelhs.org>
2021-01-08 02:24:43 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+   int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+  for ( int a = 9; a >= 0; a--){
+    if(num[a] % 2 == 0){
+  System.out.println(num[a]);
+     }
+   }
+  }
+}
